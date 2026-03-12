7aa7215286225f7fd1628f0eb70c6859455c8c15
MomosakiRiku <nguyenmanhtung76@gmail.com>
2026-03-12 20:04:19 +0700

    refactor: changed code convention

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,37 +1,36 @@
 #include "kernel/types.h"
 #include "user/user.h"
-int main() {
 
-    int fd1[2]; // From parent to child
-    int fd2[2]; // From child to parent
+int 
+main() {
+
+    int fd1[2];
+    int fd2[2];
     char buffer[20];
 
-    // Create pipes
     pipe(fd1);
     pipe(fd2);
 
     if (fork() == 0) {
-        // Child process
-        close(fd1[1]); // Child's role is to read 
-        close(fd2[0]); // Child's role is to write
+        close(fd1[1]);
+        close(fd2[0]);
 
-        read(fd1[0], buffer, sizeof(buffer)); // Read from parent 
+        read(fd1[0], buffer, sizeof(buffer));
         printf("%d: %s\n", getpid(), buffer);
-        write(fd2[1], "received pong", 13); // Write to parent
+        write(fd2[1], "received pong", 13);
 
-        close(fd1[0]); // Close pipe after using
-        close(fd2[1]); // Close pipe after using
+        close(fd1[0]);
+        close(fd2[1]);
     } else {
-        // Parent process
-        close(fd1[0]); // Parent's role is to write
-        close(fd2[1]); // Parent's role is to read
+        close(fd1[0]);
+        close(fd2[1]);
 
-        write(fd1[1], "received ping", 13); // Write to child
-        read(fd2[0], buffer, sizeof(buffer)); // Read from child
+        write(fd1[1], "received ping", 13);
+        read(fd2[0], buffer, sizeof(buffer));
         printf("%d: %s\n", getpid(), buffer); 
 
-        close(fd1[1]); // Close pipe after using
-        close(fd2[0]); // Close pipe after using
+        close(fd1[1]);
+        close(fd2[0]); 
     }
     exit(0);
 }
